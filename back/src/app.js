775fd77f0c1b52b120a3d3270f4c8b2a5accbd15
Gustavo-de-Brito <gustavoaraujo086@gmail.com>
2022-06-19 20:15:13 -0300

    feat: retorna um array com os dez ultimos tweets ao fazer o get na rota '/tweets'

--- back/src/app.js
+++ back/src/app.js
@@ -21,13 +21,23 @@ app.post("/tweets", (req, res) => {
   const tweet = req.body;
   const { avatar } = users.find( user => user.username === tweet.username);
 
-  tweets.push({ ...tweet, avatar });=
+  tweets.push({ ...tweet, avatar });
 
   res.send("OK");
 });
 
 app.get("/tweets", (req, res) => {
-  
+  const lastTweets = [];
+
+  for(let i = (tweets.length - 1); i >= 0; i--) {
+    lastTweets.push(tweets[i]);
+
+    if(lastTweets.length === 10) {
+      break;
+    }
+  }
+
+  res.send(lastTweets);
 });
 
 app.listen(5000);
